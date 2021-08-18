afa70c8ce4b716e83115dcb9ff3533e18490839e
Vladislav Tsybulya <vladmir909@mail.ru>
2021-08-18 22:47:20 +0800

    #17 Server and server deployment
    * Add .gitignore
    
    * #11 Add python script for iperf starting
    
    * #9 Add info from environment variables
    
    * Add typization for arguments
    
    * Change logging method
    
    * Clean code
    
    * Update arguments
    
    * Fix typo
    
    * Fix github action
    
    * Fix action
    
    * #7 #8 Add server to start iPerf from application
    
    Added server.py script to launch iPerf with parameters that the phone application sends.
    The start_iperf.py script has been renamed to iperf_wrapper.py and redesigned as a class.
    
    * Fix actions double compiling
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Add Dockerfile
    
    * Fix missing requirements.txt in docker
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update Dockerfile
    
    * Update docker.yml
    
    * Update Dockerfile
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Refactor docker.yml
    
    * Update docker.yml
    
    * Change docker system to ubuntu
    
    * Fix Dockerfile
    
    * Change host and port for run Flask server
    
    * Clean code
    
    * Delete start_iperf.py
    
    * Update build status link in README.md
    
    * Update docker.yml
    
    * Update docker.yml
    
    * Update Dockerfile
    
    * Update README.md
    
    Co-authored-by: Alexei <mail@bykov-alexei.ru>
    Co-authored-by: Bykov Alexei <51323720+bykov-alexei@users.noreply.github.com>

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request
+from iperf_wrapper import Iperf_wrapper
+
+
+app = Flask(__name__)
+
+iperf: Iperf_wrapper = Iperf_wrapper(verbose=True)
+
+
+@app.route("/start-iperf", methods=['GET'])
+def start_iperf_binary():
+    global iperf
+
+    iperf_parameters = request.args.get("args")
+    if iperf_parameters is not None:
+        iperf.iperf_parameters = iperf_parameters
+
+    status = iperf.start()
+    if status:
+        return f"iPerf started with parameters {iperf.iperf_parameters}"
+
+    iperf.stop()
+    status = iperf.start()
+    if status:
+        return f"iPerf restarted with parameters {iperf.iperf_parameters}"
+
+    return "Error"
+
+
+@app.route("/stop-iperf", methods=['GET'])
+def stop_iperf():
+    global iperf
+
+    if iperf.is_started:
+        status = iperf.stop()
+        return str(f"iPerf stopped with status {status}")
+
+    return "iPerf already stopped"
+
+app.run(host="0.0.0.0", port="5000")
